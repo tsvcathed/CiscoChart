cda752af6ff784bf23cf2105bc76d9517d1fd5c5
Peter Godwin <pgodwin@gmail.com>
2015-10-25 14:14:24 +1000

    New WebAPI Version
    As an experiment, ported to MVC Web API.
    Lot of junk as I just used the Visual Studio template.

---
+++ WebService/WebService/Models/Interface.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService.Models
+{
+
+    public class Interface
+    {
+        /// <summary>
+        /// Interface Oid number for queries
+        /// </summary>
+        public int Oid { get; set; }
+        /// <summary>
+        /// Interface Id/Name
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Interface Name/Description
+        /// </summary>
+        public string Name { get; set; }
+    }
+
+}
---
+++ WebService/WebService/Models/InterfaceThroughput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebService.Models
+{
+
+    public class InterfaceThroughput
+    {
+        /// <summary>
+        /// Ingress in bps
+        /// </summary>
+        public Int64 Ingress { get; set; }
+
+        /// <summary>
+        /// Egress in bps
+        /// </summary>
+        public Int64 Egress { get; set; }
+
+        /// <summary>
+        /// When should the request be made again? In seconds (not ms).
+        /// </summary>
+        public int Interval { get; set; }
+
+        /// <summary>
+        /// Time of the request
+        /// </summary>
+        public DateTime Time { get; set; }
+    }
+}
